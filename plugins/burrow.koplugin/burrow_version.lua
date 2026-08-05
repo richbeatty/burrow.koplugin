d792f59289c24ddf5c1da9fae22807e8270a65ab
richbeatty <99283667+richbeatty@users.noreply.github.com>
2026-08-05 11:45:00 -0400

    Bump Burrow to 0.2.2 alpha

--- plugins/burrow.koplugin/burrow_version.lua
+++ plugins/burrow.koplugin/burrow_version.lua
@@ -1,6 +1,6 @@
 return {
-    VERSION = "0.2.1-alpha",
-    DISPLAY_VERSION = "0.2.1 alpha",
+    VERSION = "0.2.2-alpha",
+    DISPLAY_VERSION = "0.2.2 alpha",
     STORE_ENGINE_VERSION = "1.2.0",
 
     -- Burrow is tested against KOReader 2026.07.1. Older releases are blocked,
