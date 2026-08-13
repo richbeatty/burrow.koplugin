789c9248c232911d49014ae0cebdd66275e86b33
richbeatty <99283667+richbeatty@users.noreply.github.com>
2026-08-13 10:16:46 -0400

    Start automatic update scheduler module

--- plugins/burrow.koplugin/burrow_updater_auto.lua
+++ plugins/burrow.koplugin/burrow_updater_auto.lua
@@ -1,1 +1,20 @@
-return {}
+local NetworkMgr = require("ui/network/manager")
+local UIManager = require("ui/uimanager")
+local logger = require("logger")
+local _ = require("l10n.gettext")
+
+local AutoUpdater = {}
+
+local AUTO_SETTING = "burrow_auto_update_check"
+local LAST_CHECK_SETTING = "burrow_update_last_check"
+local RETRY_INTERVAL = 60 * 60
+
+function AutoUpdater.apply(Updater)
+    if not Updater or Updater._burrow_auto_scheduler_v2 then
+        return Updater
+    end
+    Updater._burrow_auto_scheduler_v2 = true
+    return Updater
+end
+
+return AutoUpdater
