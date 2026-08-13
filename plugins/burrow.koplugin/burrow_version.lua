acdf3cbbd888b088271e26e1f8e3cadd091e9e78
richbeatty <99283667+richbeatty@users.noreply.github.com>
2026-08-13 10:19:39 -0400

    Set version 0.4.2-beta.9

--- plugins/burrow.koplugin/burrow_version.lua
+++ plugins/burrow.koplugin/burrow_version.lua
@@ -1,6 +1,6 @@
 return {
-    VERSION = "0.4.2-beta.8",
-    DISPLAY_VERSION = "0.4.2-beta.8",
+    VERSION = "0.4.2-beta.9",
+    DISPLAY_VERSION = "0.4.2-beta.9",
     STORE_ENGINE_VERSION = "1.2.1",
 
     -- Burrow is tested against KOReader 2026.07.2. Older releases are blocked,
