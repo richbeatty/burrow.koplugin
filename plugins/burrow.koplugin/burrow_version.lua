6a9aa600492bea784614afb15ff2399a720f22ae
richbeatty <99283667+richbeatty@users.noreply.github.com>
2026-08-14 13:35:16 -0400

    Prepare Burrow 0.4.3-beta.5

--- plugins/burrow.koplugin/burrow_version.lua
+++ plugins/burrow.koplugin/burrow_version.lua
@@ -1,6 +1,6 @@
 return {
-    VERSION = "0.4.3-beta.4",
-    DISPLAY_VERSION = "0.4.3-beta.4",
+    VERSION = "0.4.3-beta.5",
+    DISPLAY_VERSION = "0.4.3-beta.5",
     STORE_ENGINE_VERSION = "1.2.1",
 
     -- Burrow is tested against KOReader 2026.07.2. Older releases are blocked,
